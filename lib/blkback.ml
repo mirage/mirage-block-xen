50912a8bf4342fffa0f17c40c6f36f1589f49503
David Scott <dave.scott@docker.com>
2016-03-13 12:01:11 +0000

    Remove `lwt x = e1 in e2` and replace with `>>=`
    Signed-off-by: David Scott <dave.scott@docker.com>

--- lib/blkback.ml
+++ lib/blkback.ml
@@ -105,7 +105,7 @@ module BlockError = struct
 end
 
 let is_writable req = match req.Req.op with
-| Some Req.Read -> true (* we need to write into the page *) 
+| Some Req.Read -> true (* we need to write into the page *)
 | Some Req.Write -> false (* we read from the guest and write to the backend *)
 | None ->
   printf "FATAL: unknown request type\n%!";
@@ -152,7 +152,7 @@ let service_thread t stats =
         end in
     let maybe_unmap mapping =
       try
-        Opt.iter (Gnttab.unmap_exn t.xg) mapping 
+        Opt.iter (Gnttab.unmap_exn t.xg) mapping
       with e ->
         printf "FATAL: failed to unmap grant references (frontend will be confused (%s)\n%!" (Printexc.to_string e) in
 
@@ -235,13 +235,14 @@ let service_thread t stats =
       let rec work remaining = match pop remaining with
       | [], _ -> return ()
       | now, later ->
-        lwt () = Lwt_list.iter_p (fun r ->
-          lwt result =
-            try_lwt
-              lwt () = (if r.op = Req.Read then t.ops.read else t.ops.write) r.sector r.buffers in
+        Lwt_list.iter_p (fun r ->
+          ( try_lwt
+              (if r.op = Req.Read then t.ops.read else t.ops.write) r.sector r.buffers
+              >>= fun () ->
               return Res.OK
             with e ->
-              return Res.Error in
+              return Res.Error )
+          >>= fun result ->
           let open Res in
           let ok, error = List.fold_left (fun (ok, error) id ->
             let slot = Ring.Rpc.Back.(slot t.ring (next_res_id t.ring)) in
@@ -252,9 +253,11 @@ let service_thread t stats =
           stats.total_ok <- stats.total_ok + ok;
           stats.total_error <- stats.total_error + error;
           return ()
-        ) now in
+        ) now
+        >>= fun () ->
         work later in
-      lwt () = work requests in
+      work requests
+      >>= fun () ->
 
       (* We must unmap before pushing because the frontend will attempt
          to reclaim the pages (without this you get "g.e. still in use!"
@@ -267,7 +270,8 @@ let service_thread t stats =
       if notify then Eventchn.notify t.xe t.evtchn;
       return () in
 
-    lwt next = A.after t.evtchn after in
+    A.after t.evtchn after
+    >>= fun next ->
     loop_forever next in
   loop_forever A.program_start
 
@@ -297,7 +301,7 @@ let init xg xe domid ring_info ops =
     on_cancel th (fun () ->
       let counter = ref 0 in
       Ring.Rpc.Back.ack_requests ring (fun _ -> incr counter);
-      if !counter <> 0 then printf "FATAL: before unmapping, there were %d outstanding requests on the ring. Events lost?\n%!" !(counter); 
+      if !counter <> 0 then printf "FATAL: before unmapping, there were %d outstanding requests on the ring. Events lost?\n%!" !(counter);
       let () = Gnttab.unmap_exn xg mapping in ()
     );
     th, stats
@@ -307,12 +311,14 @@ open X
 let get_my_domid client =
   immediate client (fun xs ->
     try_lwt
-      lwt domid = read xs "domid" in
+      read xs "domid"
+      >>= fun domid ->
       return (int_of_string domid)
     with Xs_protocol.Enoent _ -> return 0)
 
 let mk_backend_path client name (domid,devid) =
-  lwt self = get_my_domid client in
+  get_my_domid client
+  >>= fun self ->
   return (Printf.sprintf "/local/domain/%d/backend/%s/%d/%d" self name domid devid)
 
 let mk_frontend_path client (domid,devid) =
@@ -324,18 +330,21 @@ let writev client pairs =
   )
 
 let readv client path keys =
-  lwt options = immediate client (fun xs ->
+  immediate client (fun xs ->
     Lwt_list.map_s (fun k ->
       try_lwt
-        lwt v = read xs (path ^ "/" ^ k) in
+        read xs (path ^ "/" ^ k)
+        >>= fun v ->
         return (Some (k, v))
       with _ -> return None) keys
-  ) in
+  )
+  >>= fun options ->
   return (List.fold_left (fun acc x -> match x with None -> acc | Some y -> y :: acc) [] options)
 
 let read_one client k = immediate client (fun xs ->
   try_lwt
-    lwt v = read xs k in
+    read xs k
+    >>= fun v ->
     return (`OK v)
   with _ -> return (`Error ("failed to read: " ^ k)))
 
@@ -345,33 +354,41 @@ let exists client k = match_lwt read_one client k with `Error _ -> return false 
 
 (* Request a hot-unplug *)
 let request_close name (domid, devid) =
-  lwt client = make () in
-  lwt backend_path = mk_backend_path client name (domid,devid) in
+  make ()
+  >>= fun client ->
+  mk_backend_path client name (domid,devid)
+  >>= fun backend_path ->
   writev client (List.map (fun (k, v) -> backend_path ^ "/" ^ k, v) (Blkproto.State.to_assoc_list Blkproto.State.Closing))
 
 let force_close (domid, device) =
-  lwt client = make () in
-  lwt frontend_path = mk_frontend_path client (domid, device) in
-  write_one client (frontend_path ^ "/state") (Blkproto.State.to_string Blkproto.State.Closed) 
+  make ()
+  >>= fun client ->
+  mk_frontend_path client (domid, device)
+  >>= fun frontend_path ->
+  write_one client (frontend_path ^ "/state") (Blkproto.State.to_string Blkproto.State.Closed)
 
 let run ?(max_indirect_segments=256) t name (domid,devid) =
-  lwt client = make () in
+  make ()
+  >>= fun client ->
   let xg = Gnttab.interface_open () in
   let xe = Eventchn.init () in
 
   let open BlockError in
 
-  lwt backend_path = mk_backend_path client name (domid,devid) in
+  mk_backend_path client name (domid,devid)
+  >>= fun backend_path ->
 
   (* Tell xapi we've noticed the backend *)
-  lwt () = write_one client
+  write_one client
     (backend_path ^ "/" ^ Blkproto.Hotplug._hotplug_status)
-    Blkproto.Hotplug._online in
+    Blkproto.Hotplug._online
+  >>= fun () ->
 
-  try_lwt 
+  try_lwt
 
-    lwt info = B.get_info t in
-   
+    B.get_info t
+    >>= fun info ->
+
     (* Write the disk information for the frontend *)
     let di = Blkproto.DiskInfo.(to_assoc_list {
       sector_size = info.B.sector_size;
@@ -380,23 +397,28 @@ let run ?(max_indirect_segments=256) t name (domid,devid) =
       mode = Mode.ReadWrite }) in
     (* Advertise indirect descriptors with the same default as Linux blkback *)
     let features = Blkproto.FeatureIndirect.(to_assoc_list { max_indirect_segments}) in
-    lwt () = writev client (List.map (fun (k, v) -> backend_path ^ "/" ^ k, v) (di @ features)) in
-    lwt frontend_path = match_lwt read_one client (backend_path ^ "/frontend") with
+    writev client (List.map (fun (k, v) -> backend_path ^ "/" ^ k, v) (di @ features))
+    >>= fun () ->
+    ( match_lwt read_one client (backend_path ^ "/frontend") with
       | `Error x -> failwith x
-      | `OK x -> return x in
-   
+      | `OK x -> return x )
+    >>= fun frontend_path ->
+
     (* wait for the frontend to enter state Initialised *)
-    lwt () = wait client (fun xs ->
+    wait client (fun xs ->
       try_lwt
-        lwt state = read xs (frontend_path ^ "/" ^ Blkproto.State._state) in
+        read xs (frontend_path ^ "/" ^ Blkproto.State._state)
+        >>= fun state ->
         if Blkproto.State.of_string state = Some Blkproto.State.Initialised
         || Blkproto.State.of_string state = Some Blkproto.State.Connected
         then return ()
         else raise Xs_protocol.Eagain
       with Xs_protocol.Enoent _ -> raise Xs_protocol.Eagain
-    ) in
+    )
+    >>= fun () ->
 
-    lwt frontend = readv client frontend_path Blkproto.RingInfo.keys in
+    readv client frontend_path Blkproto.RingInfo.keys
+    >>= fun frontend ->
     let ring_info = match Blkproto.RingInfo.of_assoc_list frontend with
       | `OK x -> x
       | `Error x -> failwith x in
@@ -419,32 +441,40 @@ let run ?(max_indirect_segments=256) t name (domid,devid) =
       read = device_read;
       write = device_write;
     } in
-    lwt () = writev client (List.map (fun (k, v) -> backend_path ^ "/" ^ k, v) (Blkproto.State.to_assoc_list Blkproto.State.Connected)) in
+    writev client (List.map (fun (k, v) -> backend_path ^ "/" ^ k, v) (Blkproto.State.to_assoc_list Blkproto.State.Connected))
+    >>= fun () ->
     (* wait for the frontend to disappear or enter a Closed state *)
-    lwt () = wait client (fun xs -> 
+    wait client (fun xs ->
       try_lwt
-        lwt state = read xs (frontend_path ^ "/state") in
+        read xs (frontend_path ^ "/state")
+        >>= fun state ->
         if Blkproto.State.of_string state <> (Some Blkproto.State.Closed)
         then raise Xs_protocol.Eagain
         else return ()
       with Xs_protocol.Enoent _ ->
         return ()
-    ) in
+    )
+    >>= fun () ->
     Lwt.cancel be_thread;
     Lwt.return stats
   with e ->
     printf "blkback caught %s\n%!" (Printexc.to_string e);
-    lwt () = B.disconnect t in
+    B.disconnect t
+    >>= fun () ->
     fail e
 
 let create ?backend_domid name (domid, device) =
-  lwt client = make () in
+  make ()
+  >>= fun client ->
   (* Construct the device: *)
-  lwt backend_path = mk_backend_path client name (domid, device) in
-  lwt frontend_path = mk_frontend_path client (domid, device) in
-  lwt backend_domid = match backend_domid with
+  mk_backend_path client name (domid, device)
+  >>= fun backend_path ->
+  mk_frontend_path client (domid, device)
+  >>= fun frontend_path ->
+  ( match backend_domid with
   | None -> get_my_domid client
-  | Some x -> return x in
+  | Some x -> return x )
+  >>= fun backend_domid ->
   let c = Blkproto.Connection.({
     virtual_device = string_of_int device;
     backend_path;
@@ -457,22 +487,25 @@ let create ?backend_domid name (domid, device) =
   }) in
   transaction client (fun xs ->
     Lwt_list.iter_s (fun (owner_domid, (k, v)) ->
-      lwt () = write xs k v in
+      write xs k v
+      >>= fun () ->
       let acl =
         let open Xs_protocol.ACL in
         { owner = owner_domid; other = READ; acl = [ ] } in
-      lwt () = setperms xs k acl in
-      return ()
+      setperms xs k acl
     ) (Blkproto.Connection.to_assoc_list c)
   )
 
 let destroy name (domid, device) =
-  lwt client = make () in
-  lwt backend_path = mk_backend_path client name (domid, device) in
-  lwt frontend_path = mk_frontend_path client (domid, device) in
+  make ()
+  >>= fun client ->
+  mk_backend_path client name (domid, device)
+  >>= fun backend_path ->
+  mk_frontend_path client (domid, device)
+  >>= fun frontend_path ->
   immediate client (fun xs ->
-    lwt () = try_lwt rm xs backend_path with _ -> return () in
-    lwt () = try_lwt rm xs frontend_path with _ -> return () in
-    return ()
+    (try_lwt rm xs backend_path with _ -> return ())
+    >>= fun () ->
+    (try_lwt rm xs frontend_path with _ -> return ())
   )
 end
